8e5db706c438190756ec88cc35e8cf84d1d30b02
Fellipe Mendes <fellipe.mendes90@hotmail.com>
2019-05-29 17:02:31 -0300

    BeforeClass

--- src/test/java/RunCukesTest.java
+++ src/test/java/RunCukesTest.java
@@ -1,6 +1,11 @@
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.PickleEventWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 @CucumberOptions(
         dryRun = false,
@@ -12,10 +17,21 @@ import org.testng.annotations.DataProvider;
                 "json:target/allure-results/AllureTestReport.json",}
         )
 public class RunCukesTest extends AbstractTestNGCucumberTests {
+    private TestNGCucumberRunner testNGCucumberRunner;
 
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
         return super.scenarios();
     }
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() throws Exception {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+
+//    @Test(groups = "cucumber scenarios", description = "Runs Cucumber Scenarios", dataProvider = "scenarios")
+//    public void scenario(PickleEventWrapper pickleEvent, CucumberFeatureWrapper cucumberFeature) throws Throwable{
+//        testNGCucumberRunner.runScenario(pickleEvent.getPickleEvent());
+//    }
 }
